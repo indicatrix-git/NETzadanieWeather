0ebedfcc6f5734bdef6916f6646e4ff7dacd2de1
Martin Timothy Timko <complex.indicatrix@gmail.com>
2026-02-18 20:14:44 +0100

    Add files via upload

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using NETzadanie.Configuration;
+using Microsoft.Extensions.Options;
+using System.IO;
+
+namespace NETzadanie.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly JwtOptions _jwtOptions;
+
+        public AuthController(IOptions<JwtOptions> jwtOptions)
+        {
+            _jwtOptions = jwtOptions.Value;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login()
+        {
+            var claims = new[]
+            {
+            new Claim(ClaimTypes.Name, "testUser")
+        };
+
+            var key = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(_jwtOptions.Key));
+
+            var creds = new SigningCredentials(
+                key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _jwtOptions.Issuer,
+                audience: _jwtOptions.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(_jwtOptions.ExpireMinutes),
+                signingCredentials: creds);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return Ok(new { token = jwt });
+
+
+        }
+
+
+    }
+}
---
+++ Controllers/TemperatureController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NETzadanie.Models;
+using NETzadanie.Services;
+
+
+
+namespace NETzadanie.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class TemperatureController : ControllerBase
+    {
+        private readonly ITemperatureService _temperatureService;
+
+        public TemperatureController(ITemperatureService temperatureService)
+        {
+            _temperatureService = temperatureService;
+        }
+
+        [HttpGet("{city}")]
+        public async Task<IActionResult> Get(string city)
+        {
+            var result = await _temperatureService.GetTemperatureAsync(city);
+
+            if (result == null)
+                return StatusCode(503, "No temperature data available.");
+
+            return Ok(result);
+        }
+
+
+
+    }
+}
