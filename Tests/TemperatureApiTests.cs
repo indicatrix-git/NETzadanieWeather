6e869b3258c3704ea9e0ac2c932fafa454f723f8
Martin Timothy Timko <complex.indicatrix@gmail.com>
2026-02-18 20:13:35 +0100

    Add files via upload

---
+++ Tests/TemperatureApiTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using Xunit;
+
+namespace NETzadanie.Tests
+{
+    public class TemperatureApiTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        TemperatureApiTests(
+            WebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Unauthorized_WithoutToken()
+        {
+            var response = await _client
+                .GetAsync("/api/temperature/bratislava");
+
+            Assert.Equal(HttpStatusCode.Unauthorized,
+                response.StatusCode);
+        }
+
+
+
+    }
+}
---
+++ Tests/TemperatureServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using Moq;
+using NETzadanie.Models;
+using NETzadanie.Services;
+using System.Text.Json;
+using Xunit;
+
+namespace NETzadanie.Tests
+{
+    public class TemperatureServiceTests
+    {
+        [Fact]
+        public async Task ReturnsCachedValue_WhenWeatherApiFails()
+        {
+            var weatherMock = new Mock<IWeatherService>();
+            weatherMock.Setup(x => x.GetTemperatureAsync(It.IsAny<int>()))
+                .ThrowsAsync(new HttpRequestException());
+
+            var cache = new MemoryDistributedCache(
+                Options.Create(new MemoryDistributedCacheOptions()));
+
+            var cachedResponse = new TemperatureResponse
+            {
+                City = "bratislava",
+                TemperatureC = 10.11M,
+                MeasuredAtUtc = DateTime.UtcNow
+            };
+
+            await cache.SetStringAsync(
+                "temperature:bratislava",
+                JsonSerializer.Serialize(cachedResponse));
+
+            var service = new TemperatureService(
+                weatherMock.Object, cache);
+
+            var result = await service.GetTemperatureAsync("bratislava");
+
+            Assert.NotNull(result);
+            Assert.Equal(10.11m, result.TemperatureC);
+        }
+    }
+}
